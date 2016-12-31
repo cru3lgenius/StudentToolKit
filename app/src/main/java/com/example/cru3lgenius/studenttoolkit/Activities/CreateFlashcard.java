6f15a36414be0c5d8f0765dc4045c340b25f5680
cru3lgenius <cru3lgenius@gmail.com>
2016-12-31 18:41:22 +0200

    Added interface to the create flashcard activity

--- app/src/main/java/com/example/cru3lgenius/studenttoolkit/Activities/CreateFlashcard.java
+++ app/src/main/java/com/example/cru3lgenius/studenttoolkit/Activities/CreateFlashcard.java
@@ -1,17 +1,24 @@
 package com.example.cru3lgenius.studenttoolkit.Activities;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.cru3lgenius.studenttoolkit.Models.Flashcard;
 import com.example.cru3lgenius.studenttoolkit.R;
+import com.google.gson.Gson;
 
 import java.io.Serializable;
 
 public class CreateFlashcard extends AppCompatActivity {
     Button saveFlashcardButton;
+    public static final String FLASHCARD_PREFERENCES = "flashcardPrefs";
+    SharedPreferences prefs;
+    SharedPreferences.Editor prefEdit;
+    final Gson gson = new Gson();
     EditText answer,question;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,23 @@ public class CreateFlashcard extends AppCompatActivity {
                 }
             }
         });
+
+        prefs = getSharedPreferences(FLASHCARD_PREFERENCES,MODE_PRIVATE);
+        prefEdit = prefs.edit();
+
     }
 
     //TODO: Creates and saves a new flashcard
     private void saveFlashcard(){
-
+        String quest = "KFO STAVA";
+        String anwer = "NISHTO";
+        Flashcard card = new Flashcard();
+        card.setAnswer(anwer);
+        card.setQuestion(quest);
+        System.out.println("ORIGINAL OBJECT :" + card);
+        String jsonCard = gson.toJson(card);
+        System.out.println("AFTER GSON :" + jsonCard);
+        prefEdit.putString("idFlashcard",jsonCard);
+        prefEdit.commit();
     }
 }
--- app/src/main/java/com/example/cru3lgenius/studenttoolkit/Activities/ReviewFlashcards.java
+++ app/src/main/java/com/example/cru3lgenius/studenttoolkit/Activities/ReviewFlashcards.java
@@ -1,15 +1,24 @@
 package com.example.cru3lgenius.studenttoolkit.Activities;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.cru3lgenius.studenttoolkit.Models.Flashcard;
 import com.example.cru3lgenius.studenttoolkit.R;
+import com.google.gson.Gson;
 
 public class ReviewFlashcards extends AppCompatActivity {
-
+    final Gson gson = new Gson();
+    SharedPreferences sharedPrefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_flashcards);
+        sharedPrefs = getSharedPreferences("flashcardPrefs",MODE_PRIVATE);
+        String a = sharedPrefs.getString("idFlashcard","Default");
+        Flashcard restoredCard = gson.fromJson(a,Flashcard.class);
+        System.out.println("RESTORED FLASHCARD ANSWER: " + restoredCard.getAnswer());
+
     }
 }
